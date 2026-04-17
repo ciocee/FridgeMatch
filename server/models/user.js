289cba2a68fc650624e9199adc4357e0cc235002
ciocee <valerio.cioce@gmail.com>
2026-04-17 17:03:01 +0200

    fix credo

--- server/models/user.js
+++ server/models/user.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
-    username: {
+    password: {
         type: String,
         required: true
     }
